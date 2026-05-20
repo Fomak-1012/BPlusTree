87b5ec643af3604c86531a29930fc4cba40785d2
Fomak <fomalhaut_1012@sjtu.edu.cn>
2026-05-21 00:12:23 +0800

    feat : finish Remove

--- src/storage/index/b_plus_tree.cpp
+++ src/storage/index/b_plus_tree.cpp
@@ -75,7 +75,7 @@ auto BPLUSTREE_TYPE::GetValue(const KeyType& key,
     return false;
   }
 
-  ReadPageGuard guard = bpm_ => FetchPageRead(head_guard.As<BPlusTreeHeaderPage>() -> root_page_id_);
+  ReadPageGuard guard = bpm_ -> FetchPageRead(head_guard.As<BPlusTreeHeaderPage>() -> root_page_id_);
 
   head_guard.Drop();
 
@@ -83,7 +83,7 @@ auto BPLUSTREE_TYPE::GetValue(const KeyType& key,
 
   while (!tmp_page -> IsLeafPage()) {
     auto internal = reinterpret_cast<const InternalPage*>(tmp_page);
-    int slot_num = BinaryFind(interal, key);
+    int slot_num = BinaryFind(internal, key);
 
     if (slot_num == -1) {
       return false;
@@ -172,7 +172,7 @@ auto BPLUSTREE_TYPE::Insert(const KeyType& key, const ValueType& value,
   }
 
   int idx = slot_num + 1;
-  int oldsize = leaf_page -> Getsize();
+  int oldsize = leaf_page -> GetSize();
   leaf_page -> IncreaseSize(1);
 
   for (int i = oldsize; i > idx; i--) {
@@ -211,7 +211,7 @@ auto BPLUSTREE_TYPE::Insert(const KeyType& key, const ValueType& value,
   leaf_page -> SetNextPageId(new_page_id);
 
   auto new_key = back_page -> KeyAt(0);
-  auto new_value = new_page_id_;
+  auto new_value = new_page_id;
 
   // only root
 
@@ -284,7 +284,7 @@ auto BPLUSTREE_TYPE::Insert(const KeyType& key, const ValueType& value,
   root_page -> IncreaseSize(1);
   WritePageGuard header_guard = bpm_ -> FetchPageWrite(header_page_id_);
   header_guard.template AsMut<BPlusTreeHeaderPage>() -> root_page_id_ = root_page_id;
-  
+
   return true;
 }
 
@@ -300,11 +300,304 @@ auto BPLUSTREE_TYPE::Insert(const KeyType& key, const ValueType& value,
  * necessary.
  */
 
+ /*
+ 总结一下Remove的操作：
+ 1. 先往下一路找到底，如果删了这个叶子还是 >= order / 2，就不管
+ 2. 如果 < order / 2了，就找找兄弟，如果兄弟有多的，就借一个过来，顺便把父节点改一下
+                                  如果没有多的，那正好两个直接合并了，记得改父节点。
+                                  如果这一层（第二层）被合并到只剩一个节点了，就把根节点删了，彼可取而代之
+ 3. 如果删的数和键值一样记得改！ 
+ */ 
+
 INDEX_TEMPLATE_ARGUMENTS
 void BPLUSTREE_TYPE::Remove(const KeyType& key, Transaction* txn)
 {
   //Your code here
-  
+
+  WritePageGuard head_guard = bpm_ -> FetchPageWrite(header_page_id_);
+
+  // empty tree
+  if (head_guard.template As<BPlusTreeHeaderPage>() -> root_page_id_ == INVALID_PAGE_ID) {
+    return ;
+  }
+
+  // easy delete
+
+  WritePageGuard guard = bpm_ -> FetchPageWrite(head_guard.As<BPlusTreeHeaderPage>() -> root_page_id_);
+
+  head_guard.Drop();
+  std::deque<WritePageGuard> fathers;
+  auto tmp_page = guard.template AsMut<BPlusTreePage>();
+
+  while (!tmp_page -> IsLeafPage()) {
+    auto internal = reinterpret_cast<const InternalPage*>(tmp_page);
+    int slot_num = BinaryFind(internal, key);
+    if (slot_num == -1) {
+      return ;
+    }
+
+    page_id_t child_page_id = reinterpret_cast<const InternalPage*>(tmp_page) -> ValueAt(slot_num);
+    fathers.push_back(std::move(guard));
+    guard = bpm_ -> FetchPageWrite(child_page_id);
+    
+    tmp_page = guard.template AsMut<BPlusTreePage>();
+  }
+
+  auto* leaf_page = reinterpret_cast<LeafPage*>(tmp_page); 
+  int slot_num = BinaryFind(leaf_page, key);
+
+  if (slot_num == -1 || comparator_(leaf_page -> KeyAt(slot_num), key) != 0) {
+    return ;
+  }
+
+  int idx = slot_num;
+  int old_size = leaf_page -> GetSize();
+
+  for (int i = idx; i < old_size - 1; i++) {
+    leaf_page -> SetKeyAt(i, leaf_page -> KeyAt(i + 1));
+    leaf_page -> SetValueAt(i, leaf_page -> ValueAt(i + 1));
+  }
+
+  leaf_page -> IncreaseSize(-1);
+  int min_size = leaf_page -> GetMinSize();
+  int size = leaf_page -> GetSize();
+
+  if (size >= min_size) return ;
+
+  // consider merge
+
+  page_id_t current_page_id = guard.PageId();
+  bool current_is_leaf = true;
+  WritePageGuard current_guard = std::move(guard);
+
+   while (1){
+    auto current_page = current_guard.template AsMut<BPlusTreePage>();
+
+    if (fathers.empty()) {
+      WritePageGuard header_guard = bpm_ -> FetchPageWrite(header_page_id_);
+      if (current_is_leaf) {
+        if (current_page ->GetSize() == 0) {
+          header_guard.template AsMut<BPlusTreeHeaderPage>() -> root_page_id_ = INVALID_PAGE_ID;
+        }
+        return ;
+      }
+
+      if (current_page -> GetSize() == 0) {
+        header_guard.template AsMut<BPlusTreeHeaderPage>() ->root_page_id_ = INVALID_PAGE_ID;
+      }
+      else {
+        header_guard.template AsMut<BPlusTreeHeaderPage>() ->root_page_id_ = current_page_id;
+      }
+      return ;
+    }
+
+    auto father_guard = std::move(fathers.back());
+    fathers.pop_back();
+    auto father = father_guard.template AsMut<BPlusTreePage>();
+    auto* father_page = reinterpret_cast<InternalPage*>(father);
+    int current_idx = -1;
+    for (int i = 0; i < father_page -> GetSize(); i++) {
+      if (father_page -> ValueAt(i) == current_page_id) {
+        current_idx = i;
+        break;
+      }
+    }
+    int father_size = father_page -> GetSize();
+
+    if (current_is_leaf) {
+      auto* current_leaf = reinterpret_cast<LeafPage*>(current_page);
+
+      // have left sibling 
+
+      if (current_idx > 0) {
+        page_id_t left_page_id = father_page -> ValueAt(current_idx - 1);
+        auto left_guard = bpm_ -> FetchPageWrite(left_page_id);
+        auto* left_page = left_guard.template AsMut<LeafPage>();
+        
+        // left sibling has surplus elements
+
+        if (left_page -> GetSize() > left_page -> GetMinSize()) {
+          int current_size = current_leaf -> GetSize();
+          int left_size = left_page -> GetSize();
+          current_leaf -> IncreaseSize(1);
+          for (int i = current_size; i > 0; i--) {
+            current_leaf -> SetKeyAt(i, current_leaf -> KeyAt(i - 1));
+            current_leaf -> SetValueAt(i, current_leaf -> ValueAt(i - 1));
+          }
+          current_leaf -> SetKeyAt(0, left_page -> KeyAt(left_size - 1));
+          current_leaf -> SetValueAt(0, left_page -> ValueAt(left_size - 1));
+          left_page -> IncreaseSize(-1);
+          father_page -> SetKeyAt(current_idx, current_leaf -> KeyAt(0));
+          return ;
+        }
+      }
+
+      // don't have left sibling or left sibling has no surplus elements
+      // but have right sibling
+
+      if (current_idx + 1 < father_size) {
+        page_id_t right_page_id = father_page -> ValueAt(current_idx + 1);
+        auto right_guard = bpm_ -> FetchPageWrite(right_page_id);
+        auto* right_page = right_guard.template AsMut<LeafPage>();
+
+        // right sibling have surplus elements
+
+        if (right_page -> GetSize() > right_page -> GetMinSize()) {
+          int current_size = current_leaf -> GetSize();
+          current_leaf -> SetKeyAt(current_size, right_page -> KeyAt(0));
+          current_leaf -> SetValueAt(current_size, right_page -> ValueAt(0));
+          current_leaf -> IncreaseSize(1);
+          int right_size = right_page -> GetSize();
+          for (int i = 0; i < right_size - 1; i++) {
+            right_page -> SetKeyAt(i, right_page -> KeyAt(i + 1));
+            right_page -> SetValueAt(i, right_page -> ValueAt(i + 1));
+          }
+          right_page -> IncreaseSize(-1);
+          father_page -> SetKeyAt(current_idx + 1, right_page -> KeyAt(0));
+          return ;
+        }
+      }
+
+      // can' t borrow elements from left or right siblings
+      // but have a left sibling
+      // which means can be merged with left sibling
+      if (current_idx > 0) {
+        page_id_t left_page_id = father_page -> ValueAt(current_idx - 1);
+        auto left_guard = bpm_ -> FetchPageWrite(left_page_id);
+        auto* left_page = left_guard.template AsMut<LeafPage>();
+        int left_size = left_page -> GetSize();
+        int current_size = current_leaf -> GetSize();
+        for (int i = 0; i < current_size; i++) {
+          left_page -> SetKeyAt(left_size + i, current_leaf -> KeyAt(i));
+          left_page -> SetValueAt(left_size + i, current_leaf -> ValueAt(i));
+        }
+        left_page -> IncreaseSize(current_size);
+        left_page -> SetNextPageId(current_leaf -> GetNextPageId());
+        for (int i = current_idx; i < father_size - 1; i++) {
+          father_page -> SetKeyAt(i, father_page -> KeyAt(i + 1));
+          father_page -> SetValueAt(i, father_page -> ValueAt(i + 1));
+        }
+        father_page -> IncreaseSize(-1);
+      } 
+      // if don't have a left sibling, must have a right sibling 
+      // (otherwise this layer should be delete)
+      else {
+        page_id_t right_page_id = father_page -> ValueAt(current_idx + 1);
+        auto right_guard = bpm_ -> FetchPageWrite(right_page_id);
+        auto* right_page = right_guard.template AsMut<LeafPage>();
+        int current_size = current_leaf -> GetSize();
+        int right_size = right_page -> GetSize();
+        for (int i = 0; i < right_size; i++) {
+          current_leaf -> SetKeyAt(current_size + i, right_page -> KeyAt(i));
+          current_leaf -> SetValueAt(current_size + i, right_page -> ValueAt(i));
+        }
+        current_leaf -> IncreaseSize(right_size);
+        current_leaf -> SetNextPageId(right_page -> GetNextPageId());
+        for (int i = current_idx + 1; i < father_size - 1; i++) {
+          father_page -> SetKeyAt(i, father_page -> KeyAt(i + 1));
+          father_page -> SetValueAt(i, father_page -> ValueAt(i + 1));
+        }
+        father_page -> IncreaseSize(-1);
+      }
+
+      current_page_id = father_guard.PageId();
+      current_guard = std::move(father_guard);
+      current_is_leaf = false;
+      continue;
+    }
+
+    auto* current_internal = reinterpret_cast<InternalPage*>(current_page);
+
+    if (current_internal -> GetSize() >= current_internal -> GetMinSize()) {
+      return;
+    }
+
+    if (current_idx > 0) {
+      page_id_t left_page_id = father_page -> ValueAt(current_idx - 1);
+      auto left_guard = bpm_ -> FetchPageWrite(left_page_id);
+      auto* left_page = left_guard.template AsMut<InternalPage>();
+      if (left_page -> GetSize() > left_page -> GetMinSize()) {
+        int current_size = current_internal -> GetSize();
+        int left_size = left_page -> GetSize();
+        current_internal -> IncreaseSize(1);
+        for (int i = current_size; i > 0; i--) {
+          current_internal -> SetKeyAt(i, current_internal -> KeyAt(i - 1));
+          current_internal -> SetValueAt(i, current_internal -> ValueAt(i - 1));
+        }
+        KeyType old_left_key = left_page -> KeyAt(left_size - 1);
+        current_internal -> SetValueAt(0, left_page -> ValueAt(left_size - 1));
+        current_internal -> SetKeyAt(1, father_page -> KeyAt(current_idx));
+        left_page -> IncreaseSize(-1);
+        father_page -> SetKeyAt(current_idx, old_left_key);
+        return;
+      }
+    }
+
+    if (current_idx + 1 < father_size) {
+      page_id_t right_page_id = father_page -> ValueAt(current_idx + 1);
+      auto right_guard = bpm_ -> FetchPageWrite(right_page_id);
+      auto* right_page = right_guard.template AsMut<InternalPage>();
+      if (right_page -> GetSize() > right_page -> GetMinSize()) {
+        int current_size = current_internal -> GetSize();
+        KeyType old_right_key_1 = right_page -> KeyAt(1);
+        current_internal -> SetKeyAt(current_size, father_page -> KeyAt(current_idx + 1));
+        current_internal -> SetValueAt(current_size, right_page -> ValueAt(0));
+        current_internal -> IncreaseSize(1);
+        int right_size = right_page -> GetSize();
+        right_page -> SetValueAt(0, right_page -> ValueAt(1));
+        for (int i = 1; i < right_size - 1; i++) {
+          right_page -> SetKeyAt(i, right_page -> KeyAt(i + 1));
+          right_page -> SetValueAt(i, right_page -> ValueAt(i + 1));
+        }
+        right_page -> IncreaseSize(-1);
+        father_page -> SetKeyAt(current_idx + 1, old_right_key_1);
+        return;
+      }
+    }
+
+    if (current_idx > 0) {
+      page_id_t left_page_id = father_page -> ValueAt(current_idx - 1);
+      auto left_guard = bpm_ -> FetchPageWrite(left_page_id);
+      auto* left_page = left_guard.template AsMut<InternalPage>();
+      int left_size = left_page -> GetSize();
+      int current_size = current_internal -> GetSize();
+      left_page -> SetKeyAt(left_size, father_page -> KeyAt(current_idx));
+      left_page -> SetValueAt(left_size, current_internal -> ValueAt(0));
+      for (int i = 1; i < current_size; i++) {
+        left_page -> SetKeyAt(left_size + i, current_internal -> KeyAt(i));
+        left_page -> SetValueAt(left_size + i, current_internal -> ValueAt(i));
+      }
+      left_page -> IncreaseSize(current_size);
+      for (int i = current_idx; i < father_size - 1; i++) {
+        father_page -> SetKeyAt(i, father_page -> KeyAt(i + 1));
+        father_page -> SetValueAt(i, father_page -> ValueAt(i + 1));
+      }
+      father_page -> IncreaseSize(-1);
+    } 
+    else {
+      page_id_t right_page_id = father_page -> ValueAt(current_idx + 1);
+      auto right_guard = bpm_ -> FetchPageWrite(right_page_id);
+      auto* right_page = right_guard.template AsMut<InternalPage>();
+      int current_size = current_internal -> GetSize();
+      int right_size = right_page -> GetSize();
+      current_internal -> SetKeyAt(current_size, father_page -> KeyAt(current_idx + 1));
+      current_internal -> SetValueAt(current_size, right_page -> ValueAt(0));
+      for (int i = 1; i < right_size; i++) {
+        current_internal -> SetKeyAt(current_size + i, right_page -> KeyAt(i));
+        current_internal -> SetValueAt(current_size + i, right_page -> ValueAt(i));
+      }
+      current_internal -> IncreaseSize(right_size);
+      for (int i = current_idx + 1; i < father_size - 1; i++) {
+        father_page -> SetKeyAt(i, father_page -> KeyAt(i + 1));
+        father_page -> SetValueAt(i, father_page -> ValueAt(i + 1));
+      }
+      father_page -> IncreaseSize(-1);
+    }
+
+    current_page_id = father_guard.PageId();
+    current_guard = std::move(father_guard);
+    current_is_leaf = false;
+  }
 }
 
 /*****************************************************************************
